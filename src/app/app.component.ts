5923681eb36e0c5e7acc4a75eda73d80d0d89c6b
Asadbek <71205691+AsadbekAbdullayev@users.noreply.github.com>
2025-09-14 17:35:42 +0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { BookingComponent } from './booking/booking.component';
+
 @Component({
   selector: 'app-root',
   imports: [BookingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
+  
 export class AppComponent {
   title = 'hairdresser-booking';
 }
